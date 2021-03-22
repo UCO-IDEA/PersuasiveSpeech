c9093bed06ce57950089b2e59beb65102469cd29
Robert Wall <rwall3@uco.edu>
2021-03-22 13:25:39 -0500

    footer logo update

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <div class="col-sm-12 footer">
-    <img class="footer_logo" src="../img/logo/UCO_White.svg" alt="UCO">
-    <img class="footer_logo" src="../img/logo/IDEA_White.svg" alt="CeCE IDEA">
+    <img class="footer_logo" src="img/UCO_White.svg" alt="UCO">
+    <img class="footer_logo" src="img/IDEA_White.svg" alt="CeCE IDEA">
 </div>
