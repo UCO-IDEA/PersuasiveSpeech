483abe460a161de4bf75b1457ba9f61c282c7b65
Robert Wall <rwall3@uco.edu>
2021-03-22 13:17:33 -0500

    Added Boostrap path

--- head.php
+++ head.php
@@ -1,4 +1,4 @@
-<link href="https://cece.uco.edu/idea/css/bootstrap-4.1.3.min.css"  rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css?family=Lato|Oswald|Raleway|Roboto|Source+Sans+Pro" rel="stylesheet">
 <link href="css/style.css" rel="stylesheet">
